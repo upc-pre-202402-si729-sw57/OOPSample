2cb550edc11f562d5ade143ea5e52224e9d8db84
Angel Velasquez <pcisavel@upc.edu.pe>
2024-08-21 16:00:45 -0500

    feat(sales-order): added sales order status.

---
+++ src/main/java/com/acme/sample/sales/domain/model/aggregates/SalesOrderStatus.java
@@ -0,0 +1,10 @@
+package com.acme.sample.sales.domain.model.aggregates;
+
+public enum SalesOrderStatus {
+    CREATED,
+    APPROVED,
+    IN_PROGRESS,
+    CANCELLED,
+    SHIPPED,
+    DELIVERED
+}
